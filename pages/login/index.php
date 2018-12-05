bd1ec748fbd96c331ebff19a93e5fef9f53aed69
fjose123 <fernando.jose.santos@hotmail.com>
2018-12-04 22:24:43 -0200

    alterações frontend

--- pages/login/index.php
+++ pages/login/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Log in</title>
+  <title>SISGES | Login</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -39,7 +39,7 @@
 
     <form action="../../php/control/login.php" method="post">
       <div class="form-group has-feedback">
-        <input name="user" type="text" class="form-control" placeholder="Usuario">
+        <input name="user" type="text" class="form-control" placeholder="Usuário">
         <span class="fa fa-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
